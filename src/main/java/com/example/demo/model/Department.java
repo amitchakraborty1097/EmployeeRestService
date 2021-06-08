c8b1fe21d657d515a937adf7c2d1e1aee203efb5
Amit Chakraborty <amitchakraborty1097@gmail.com>
2021-06-08 14:34:04 +0530

    modified dept

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -4,5 +4,18 @@ public class Department {
 
 	private int deptId;
 	private int deptName;
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public int getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(int deptName) {
+		this.deptName = deptName;
+	}
+	
 	
 }
